43ed74115241839c66586bd727697035397c3b8d
Carter-Rogers <37035444+Carter-Rogers@users.noreply.github.com>
2021-09-30 21:32:02 -0600

    Token encryption introduced.
    AES-128 bit encryption added to protect discord token!

--- src/main/java/me/c1tad31/chb/Bot.java
+++ src/main/java/me/c1tad31/chb/Bot.java
@@ -14,33 +14,70 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import javax.security.auth.login.LoginException;
 import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 
 public class Bot {
     public static void main(String[] args) throws IOException, LoginException {
-        File file = new File("C:/Users/Carter/Desktop/ticket.txt");
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        //File file = new File("C:/Users/Carter/Desktop/ticket.txt");
+        
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Bot.class.getResourceAsStream("config/token.txt")));
         String token;
 
         while ((token = bufferedReader.readLine()) != null) {
 
-            JDA jda = JDABuilder.createDefault(token).build();
-            CommandClientBuilder commandClientBuilder = new CommandClientBuilder();
-            commandClientBuilder.setPrefix("-");
-            commandClientBuilder.setHelpWord("helpme");
-            commandClientBuilder.setOwnerId("892899456574435348");
+           
+            Cipher cipher = null;
+            try {
+                String key = "ABCDFEGHIJKLMNOP";
+                Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
 
-            commandClientBuilder.addCommands(new HelpCommand());
-            commandClientBuilder.addCommands(new KickCommand());
-            commandClientBuilder.addCommands(new BanCommand());
-            commandClientBuilder.addCommand(new MuteCommand());
-            commandClientBuilder.addCommand(new UnmuteCommand());
-            commandClientBuilder.addCommand(new MoveVCCommand());
+                cipher = Cipher.getInstance("AES");
+                cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+                byte[] encrypted = cipher.doFinal(token.getBytes());
+                
+                StringBuilder sb = new StringBuilder();
+                for(byte b : encrypted) {
+                    sb.append((char)b);
+                }
+                String enc = sb.toString();
+                
+                byte[] bb = new byte[enc.length()];
+                for(int i = 0; i < enc.length(); i++) {
+                    bb[i] = (byte) enc.charAt(i);
+                }
 
+                cipher.init(Cipher.DECRYPT_MODE, aesKey);
 
-            CommandClient client = commandClientBuilder.build();
-            jda.addEventListener(client);
-           // jda.addEventListener(new MessageEvent());
-            jda.addEventListener(new BotReadyEvent());
+                System.out.println();
+
+                JDA jda = JDABuilder.createDefault(new String(cipher.doFinal(bb))).build();
+                CommandClientBuilder commandClientBuilder = new CommandClientBuilder();
+                commandClientBuilder.setPrefix("-");
+                commandClientBuilder.setHelpWord("helpme");
+                commandClientBuilder.setOwnerId("892899456574435348");
+    
+                commandClientBuilder.addCommands(new HelpCommand());
+                commandClientBuilder.addCommands(new KickCommand());
+                commandClientBuilder.addCommands(new BanCommand());
+                commandClientBuilder.addCommand(new MuteCommand());
+                commandClientBuilder.addCommand(new UnmuteCommand());
+                commandClientBuilder.addCommand(new MoveVCCommand());
+    
+    
+                CommandClient client = commandClientBuilder.build();
+                jda.addEventListener(client);
+               // jda.addEventListener(new MessageEvent());
+                jda.addEventListener(new BotReadyEvent());
+
+            } catch (Exception e) {
+             
+                e.printStackTrace();
+            }
+
         }
         bufferedReader.close();
     }
